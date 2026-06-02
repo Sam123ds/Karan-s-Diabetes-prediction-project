d2d358b8a98cb05b61751c1c09d0f5dd775a32e0
Sam123ds <sameer.warsolkar@itvedant.com>
2026-06-02 20:50:25 +0530

    Update karan_app2.py

--- karan_app2.py
+++ karan_app2.py
@@ -7,7 +7,6 @@ ss = joblib.load("scaler.joblib")
 Karan_model = joblib.load("Karan_model.joblib")
 
 
-st.title("Karans Diabetes Prediction Portal")
 Pregnancies = st.number_input("Enter No. of Pregnancies",0,17,1)
 Glucose = st.number_input("Enter Glucose level",0,17,5)
 BloodPressure = st.number_input("Enter your BloodPressure",69,120,80)
